c652789e08242ec2a6e276d95d139549076f13b5
up1 <somkiat.p@gmail.com>
2023-11-01 11:06:33 +0700

    Add instruction

---
+++ day01/README.md
@@ -0,0 +1,13 @@
+# Workshop with Playwright
+
+
+## Run test
+```
+$npx playwright test
+$npx playwright show-report
+```
+
+## Run test in UI Mode and watch all testcases
+```
+$npx playwright test --ui
+```
--- day01/playwright.config.ts
+++ day01/playwright.config.ts
@@ -14,7 +14,6 @@ export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
-
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
