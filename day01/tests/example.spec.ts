8536b32cb3a199a2e5bde5fc3c67eeedcf1147f0
up1 <somkiat.p@gmail.com>
2023-11-01 11:56:30 +0700

    Use env

--- day01/tests/example.spec.ts
+++ day01/tests/example.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto(process.env.BASE_URL || 'https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
