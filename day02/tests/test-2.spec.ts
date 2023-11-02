97e9ba2bd9fcf6e9a985b597c8a0084626a8e2aa
up1 <somkiat.p@gmail.com>
2023-11-02 15:42:36 +0700

    Demo with test step

--- day02/tests/test-2.spec.ts
+++ day02/tests/test-2.spec.ts
@@ -4,26 +4,32 @@ test('Case 02 @contact', async ({ page }) => {
 
   await test.step('Go to https://www.krungsri.com/th/personal', async () => {
     await page.goto('https://www.krungsri.com/th/personal');
-    // await page.locator('#cmp-btn-granted').getByText('ตกลงทั้งหมด').click();
+    await expect(page.locator('#cmp-btn-granted')).toBeVisible()
+    await page.locator('#cmp-btn-granted').click();
+
   }, { box: true });
 
   await test.step('Click ติดต่อเรา', async () => {
     const contactLink = await page.locator('xpath=//*[@id="form"]/div[4]/div/div/div[1]/div[4]/div[1]/a[5]');
+    await expect(contactLink).toBeVisible()
     await contactLink.click();
+    const title = await page.locator('xpath=//*[@id="p_lt_ctl07_pageplaceholder_p_lt_ctl04_KSX_ContactUs_lblTitle"]');
+    await expect(title).toBeVisible()
+    await expect(title).toContainText('แบบฟอร์มการติดต่อ')
+  }, { box: true });
 
-    await expect(page.getByRole('heading', { name: 'ติดต่อเรา' })).toBeVisible()
-    await page.getByText('แบบฟอร์มการติดต่อ').click();
-    await page.locator('#zoneTopic').getByText('กรุณาเลือกหัวข้อที่ต้องการสอบถาม').nth(1).click();
-    await page.locator('div').filter({ hasText: /^ผลิตภัณฑ์ด้านการลงทุน$/ }).click();
-    await page.getByText('กรุณาเลือกหัวข้อที่ต้องการสอบถาม').nth(3).click();
-    await page.locator('div').filter({ hasText: /^ทั่วไป$/ }).click();
-    await page.locator('#zoneTitle i').click();
-    await page.locator('div').filter({ hasText: /^นาย$/ }).click();
-    await page.getByPlaceholder('กรุณากรอกชื่อ').fill('fff');
-    await page.getByPlaceholder('กรุณากรอกนามสกุล').click();
-    await page.getByPlaceholder('กรุณากรอกนามสกุล').fill('hhhh');
-    await page.getByText('Drop files here to upload').click();
-    await page.getByRole('button', { name: 'ส่งข้อมูล' }).click();
+  await test.step('กรอกแบบฟอร์มการติดต่อ', async () => {
+    await expect(page.getByText('แบบฟอร์มการติดต่อ')).toBeVisible()
+    const zoneTopic = await page.locator('xpath=//*[@id="zoneTopic"]/div');
+    await expect(zoneTopic).toBeVisible()
+    await zoneTopic.click();
+  
+    const topic = await page.locator('xpath=//*[@id="zoneTopic"]/div/div[2]');
+    await expect(topic).toBeVisible()
+    const topic3 = await page.locator('xpath=//*[@id="zoneTopic"]/div/div[2]/div[4]');
+    await expect(topic3).toBeVisible()
+    await topic3.click();
+    
   }, { box: true });
 
 });
